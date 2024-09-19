9d2a8107d3f53c53b6a9698e279adc5844b49d9d
Alef <Alef@DESKTOP-ALEF>
2024-09-18 21:57:32 -0300

    Add project files.

---
+++ AcademiaDotNet.ProjetoExemplo.WindowsForms.UI/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AcademiaDotNet.ProjetoExemplo.WindowsForms.UI
+{
+    public partial class FFormularioInicial : Form
+    {
+        public FFormularioInicial()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+
+            var Fornecedor = new Fornecedor ();
+
+            if(txtNomeFornecedor.Text != string.Empty)
+            {
+                fornecedor.NomeDoFornecedor = txtNomeFornecedor.Text;
+            }
+
+            if(txtEndereco.Text != string.Empty)
+            {
+                fornecedor.Endereco = txtEndereco.Text;
+            }
+
+            if(txtNomeContato.Text != string.Empty)
+            {
+                fornecedor.NomeContato = txtNomeContato.Text;
+            }
+
+            if(txtTelefone.Text != string.Empty)
+            {
+                fornecedor.Telefone = txtTelefone.Text;
+            }
+
+            List<Fornecedor> listaFornecedores = new List<Fornecedor>();
+            listaFornecedores.Add(fornecedor);
+
+            dgvFornecedores.DataSource = listaFornecedores;
+
+        }
+        
+    }      
+
+
+    
