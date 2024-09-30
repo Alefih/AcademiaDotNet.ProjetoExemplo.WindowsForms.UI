4a68f9ad407df216159542ed259de7699fd63e32
Alef <alefrodrigues337@gmail.com>
2024-09-29 21:50:02 -0300

    Add files via upload

---
+++ CursoCSharp.ConsoleApplication.List/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ConsoleApplication.List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> nomes = new List<String>();
+
+            nomes.Add("Alef");
+            nomes.Add("Pele");
+            nomes.Add("Kaka");
+            nomes.Add("Zidane");
+            nomes.Add("Kroos");
+            nomes.Add("Iniesta");
+            nomes.Add("Gabigol");
+            nomes.Add("Messi");
+            nomes.Add("Cristiano Ronaldo");
+            nomes.Add("Neymar");
+            nomes.Add("Messi");
+
+            nomes.Sort();
+
+            foreach (String nome in nomes)
+            {
+                Console.WriteLine($"Melhores Jogadores {nome.ToUpper()}!");
+            }
+            
+
+
+
+
+        }
+    }
+}
